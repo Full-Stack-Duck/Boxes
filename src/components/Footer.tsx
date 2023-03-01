618d474f8cd037bee34aebbfa4362960a099bffe
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-02-28 23:19:12 -0300

    padding 28px (px-7)

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,7 @@
 
 export function Footer(){
     return(
-        <footer className="flex flex-col items-center bg-purple-darker text-white font-quicksand font-medium py-5">
+        <footer className="flex flex-col items-center bg-purple-darker text-white font-quicksand font-medium py-5 px-7">
             <p>Desenvolvido por fullstack.duck</p>
             <p>Direitos reservados - 2023</p>
         </footer>
