60a00a52ed523afde16389015b46945d0618acea
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-02-28 23:13:10 -0300

    ajeitando profile-icon/header tirando margin-right

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import logoIcon from '../assets/logoIcon.svg'
 
 export function Header(){
     return(
-        <header className="flex justify-between items-center bg-purple-dark w-full h-16 ">
+        <header className="flex px-7 justify-between items-center bg-purple-dark w-full h-16 ">
         <div className="w-[50%] flex items-center justify-around ">
           <a href="" className="">
             <img src={logoIcon} alt="Logomarca da Manager Box" />
@@ -12,7 +12,7 @@ export function Header(){
           <h1 className="font-viga text-2xl tracking-widest text-white">Boxes</h1>
         </div>
         <div className="w-[50%] flex items-center justify-end">
-          <a href="" className="mr-5 hover:bg-purple-400 hover:bg-opacity-10 rounded-full">
+          <a href="#" className="hover:bg-purple-400 hover:bg-opacity-10 rounded-full">
             <UserCircle size={40} weight="duotone" className="text-[#6851AC]"/>
           </a>
     
