5c7ad1d97449abc8d72df70137f0721025422a47
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-02-28 22:37:16 -0300

    FooterNav estilizado

--- src/components/FooterNav.tsx
+++ src/components/FooterNav.tsx
@@ -8,25 +8,25 @@ export function FooterNav(){
     return(
        <footer className="font-quicksand font-medium flex flex-col px-7 bg-purple-dark text-purple-light">
         <h5 className="my-7 text-white">BOXES</h5>
-        <nav className="flex flex-col my-2.5">
-            <a href="#" className={styles.aFooter}> Sua Conta</a>
-            <a href="#" className={styles.aFooter}> Contatos</a>
-            <a href="#" className={styles.aFooter}> Planos e Preços</a>
-            <a href="#" className={styles.aFooter}> Termo de Uso</a>
-            <a href="#" className={styles.aFooter}> Declaração de Uso de Cookies</a>
-            <a href="#" className={styles.aFooter}> Política de Privacidade e Segurança</a>
+        <nav className="flex flex-col my-2.5 justify-center">
+            <a href="#" className={styles.aFooter}><span className="material-symbols-outlined">chevron_right</span>Sua Conta</a>
+            <a href="#" className={styles.aFooter}><span className="material-symbols-outlined">chevron_right</span>Contatos</a>
+            <a href="#" className={styles.aFooter}><span className="material-symbols-outlined">chevron_right</span>Planos e Preços</a>
+            <a href="#" className={styles.aFooter}><span className="material-symbols-outlined">chevron_right</span>Termo de Uso</a>
+            <a href="#" className={styles.aFooter}><span className="material-symbols-outlined">chevron_right</span>Declaração de Uso de Cookies</a>
+            <a href="#" className={styles.aFooter}><span className="material-symbols-outlined">chevron_right</span>Política de Privacidade e Segurança</a>
         </nav>
-        <p className="text-white">SOCIAL</p>
-        <div className="flex flex-row">
+        <p className="text-white mb-2.5">SOCIAL</p>
+        <div className="flex gap-4 mb-9 items-center">
             <a href="#"><img src={twitterIcon} alt="logo-twitter" /></a>
             <a href="#"><img src={whatsappIcon} alt="logo-wpp" /></a>
             <a href="#"><img src={facebookIcon} alt="logo-facebook" /></a>
             <a href="#"><img src={githubIcon} alt="logo-github" /></a>
         </div>
-        <address>
+        <address className="not-italic">
             <p className="text-white">FEIRA DE SANTANA, BA</p>
-            <p>Av. Eduardo Froes da Mota, 500</p>
-            <div className="flex">
+            <p className="mt-4">Av. Eduardo Froes da Mota, 500</p>
+            <div className="flex mb-9">
                 <p>Telefone:</p>
                 <a className="text-white" href="tel:(+55)075991988661">75 9999-8888</a>
             </div>
