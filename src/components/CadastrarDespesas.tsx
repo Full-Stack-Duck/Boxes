37a56438c8f8745854f690c137e6d3ae2828f8b5
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-03-02 16:53:54 -0300

    estilizando component CadastrarDespesa

--- src/components/CadastrarDespesas.tsx
+++ src/components/CadastrarDespesas.tsx
@@ -2,27 +2,38 @@
 
 export function CadastrarDespesa(){
     return(
-        <section>
-            <div></div>
+        <section className="flex-col px-7 bg-white font-quicksand font-semibold">
+            <div className="h-px bg-purple-light my-1.5"></div>
             <div>
-                <h3></h3>
-                <p></p>
-                <article>
-                    <label htmlFor=""></label><input type="text" /><label htmlFor=""></label><input type="text" /><label htmlFor=""></label>
-                    <select name="" id="">
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                        <option value=""></option>
-                    </select><label htmlFor=""></label>
-                    <p></p>
-                    <input type="text" />
-                </article>
+                <h3 className="text-lg py-2">Cadastrar Despesas</h3>
+                <div className="text-sm text-purple-medium">
+                    <article>
+                        <label htmlFor="">NOME DA DESPESA</label>
+                        <input type="text" className="w-full border rounded border-purple-light" />
+                    </article>
+                    <article className="flex-wrap">
+                        <label className="" htmlFor="">VALOR</label>
+                        <input className="border rounded"type="number" placeholder="Ex: 3.25" />
+                        <label htmlFor="">TIPO</label>
+                        <select className="border rounded" name="" id="" >
+                            <option value="">Escolha o tipo</option>
+                            <option value="">Valor1</option>
+                            <option value="">Valor2</option>
+                            <option value="">Valor3</option>
+                        </select>
+                    </article>
+                    <article className="flex flex-wrap items-center">
+                        <label htmlFor="">DESCRIÇÃO</label>
+                        <p className="italic pl-5 text-xs text-purple-light"> (opcional) </p>
+                        <input className="w-full h-20 border rounded" type="text" placeholder="Escreva uma breve descrição da despesa..." />
+                    </article>
+                </div>
                 <div>
-                    <button>
+                    <button className="flex items-center justify-center w-full h-10 mt-2 mb-4 border-2 rounded  bg-purple-medium border-purple-dark shadow-md shadow-purple-shadow text-white"> CADASTRAR DESPESA
                         <img src="" alt="" />
                     </button>
                 </div>
+                <div className="h-px bg-purple-light my-1.5"></div>
             </div>
         </section>
     )
--- src/components/Pesquisar.tsx
+++ src/components/Pesquisar.tsx
@@ -12,7 +12,7 @@ export function Pesquisar(){
             <article className="flex-row text-sm text-purple-medium items-center">
                 <form action="">
                     <label htmlFor="">PESQUISAR POR:</label>
-                    <select className="border-2 rounded w-44 h-8 border-purple-light font-nunito ml-7 my-1.5" name="" id="">
+                    <select className="border rounded w-44 h-8 border-purple-light font-nunito ml-7 my-1.5" name="" id="">
                         <option className=" text-purple-light text-xs" value="valor0">Selecione o filtro</option>
                         <option value="valor1">Valor 1</option>
                         <option value="valor2">Valor 2 </option>
@@ -21,7 +21,7 @@ export function Pesquisar(){
                 </form>
                 <form action="">
                     <label htmlFor="">PALAVRA-CHAVE:</label>
-                    <input className="border-2 rounded w-44 h-8 border-purple-light ml-7 my-1.5" type="text" />
+                    <input className="border rounded w-44 h-8 border-purple-light ml-7 my-1.5" type="text" />
                 </form>
             </article>
             <div className="flex justify-end">
