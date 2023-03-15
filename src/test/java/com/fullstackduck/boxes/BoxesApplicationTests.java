e9514ae972cef22609a46b06dd708f81294c2fc2
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2023-03-15 20:53:36 -0300

    Iniciando back-end

---
+++ src/test/java/com/fullstackduck/boxes/BoxesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstackduck.boxes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoxesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
