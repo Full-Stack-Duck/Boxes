9473257041173450ba0a47a01b6af81778779a93
Lucas Mendes <luckasz.mendes@gmail.com>
2023-03-16 14:18:21 -0300

    Finalizando testconfig entidade Usuario

--- src/main/java/com/fullstackduck/boxes/config/TestConfig.java
+++ src/main/java/com/fullstackduck/boxes/config/TestConfig.java
@@ -9,11 +9,14 @@ import org.springframework.context.annotation.Profile;
 
 import com.fullstackduck.boxes.entities.Cliente;
 import com.fullstackduck.boxes.entities.Licenca;
+import com.fullstackduck.boxes.entities.Usuario;
+import com.fullstackduck.boxes.entities.enums.Status;
 import com.fullstackduck.boxes.entities.enums.StatusCliente;
 import com.fullstackduck.boxes.entities.enums.StatusLicenca;
 import com.fullstackduck.boxes.entities.enums.TipoLicenca;
 import com.fullstackduck.boxes.repositories.ClienteRepository;
 import com.fullstackduck.boxes.repositories.LicencaRepository;
+import com.fullstackduck.boxes.repositories.UsuarioRepository;
 
 // Classe auxiliar de configuração para o perfil de testes
 @Configuration
@@ -24,6 +27,9 @@ public class TestConfig implements CommandLineRunner{
 	private LicencaRepository licencaRepository;
 	
 	@Autowired
+	private UsuarioRepository usuarioRepository;
+	
+	@Autowired
 	private ClienteRepository clienteRepository;
 
 	@Override
@@ -31,6 +37,10 @@ public class TestConfig implements CommandLineRunner{
 		Licenca l1 = new Licenca(null, StatusLicenca.ATIVA, null, TipoLicenca.ANUAL, 98.90);
 		
 		licencaRepository.saveAll(Arrays.asList(l1));
+		
+		Usuario u1 = new Usuario(null, "Lucas", "123456789","lucas@gmail.com","7512345678", null, "Tomba", null,Status.ATIVO);
+		
+		usuarioRepository.saveAll(Arrays.asList(u1));
 		
 		Cliente c1 = new Cliente(null, "Vinicius","vinicius@gmail.com", "7512345678", null, "123456789", null,StatusCliente.ATIVA);
 		
