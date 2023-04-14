025f8cf1ec39ea7c9c00c924205b1b6c7fee84c1
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-14 13:26:01 -0300

    Tela Login concluida

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -1,34 +1,46 @@
+import logoIcon from '../assets/logoIcon.svg'
+
 export function Login(){
     return(
-        <section>
-            <article>
-                <div>
-                    <img src="" alt="" />
-                    <h1>Seja bem-vindo</h1>
-                    <h3>Por favor, insira seus dados</h3>
-                </div>
+        <section className="font-quicksand">
+            <article className=" flex flex-col justify-center items-center">
+                    <img src={logoIcon} alt="logomarca" className="w-20 h-20 mb-6 mt-10" />
+                    <h1 className="font-semibold tracking-wider text-2xl">Seja bem-vindo</h1>
+                    <h3 className="text-xs text-gray-lightgray pb-12">Por favor, insira seus dados</h3>
             </article>
-            <article>
+            <article className="flex-col mx-5">
                 <form action="">
-                    <label htmlFor="">Email</label>
-                    <input type="email" placeholder="Escreva seu email"/>
+                    <div className="py-1.5 font-medium">
+                        <label htmlFor="">Email</label>
+                        <input type="email" className="w-full h-8 border rounded border-purple-stroke placeholder:text-xs placeholder:px-3 placeholder:font-nunito" placeholder="Escreva seu email"/>
+                    </div>
 
-                    <label htmlFor="">Senha</label>
-                    <input type="password" />
+                    <div className="py-1.5 font-medium">
+                        <label htmlFor="">Senha</label>
+                        <input type="password" className="w-full h-8 border rounded border-purple-stroke"/>
+                    </div>
 
-                    <input type="checkbox" />
-                    <label htmlFor="">Lembrar minha senha</label>
+                    <div className="flex py-1.5 font-nunito text-xs">
+                        <div className="w-[50%]">
+                            <input type="checkbox" className="mr-2 border rounded border-purple-stroke"/>
+                            <label htmlFor="">Salvar senha</label>
+                        </div>
+                        <div className="w-[50%] justify-end">
+                            <a href="#" className="text-sm text-purple-medium">
+                                Esqueci minha senha
+                            </a>
+                        </div>
+                    </div>
                 </form>
-                <a href="">
-                    Esqueci minha senha
-                </a>
                 <div>
-                    <button>
+                    <button className="flex items-center justify-center w-full h-10 mt-2 mb-4 rounded-xl  bg-purple-medium shadow-md shadow-purple-shadow text-white tracking-widest">
                         Entrar
                     </button>
                 </div>
-                <p>Não tem uma conta?</p>
-                <a href="">CADASTRE-SE</a>
+                <div className="flex justify-center gap-2 py-4">
+                    <p className="text-xs text-gray-lightgray">Não tem uma conta?</p>
+                    <a href="#" className="text-sm text-purple-medium">CADASTRE-SE</a>
+                </div>
             </article>
         </section>
     )
