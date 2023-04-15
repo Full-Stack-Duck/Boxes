300857458ba02d3e2f08188d107302a20d5f8e73
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-15 18:32:02 -0300

    resultado de pesquisa de pedidos criado

---
+++ src/components/ResultadoPedido.tsx
@@ -0,0 +1,19 @@
+import verIcon from '../assets/olho-icon-on.svg'
+import editarIcon from '../assets/lapis-icon-on.svg'
+import deletarIcon from '../assets/lixeira-icon-on.svg'
+import aprovarIcon from '../assets/checkmark-circle-2.svg'
+
+
+export function ResultadoPedido(){
+    return(
+            <li className="flex mx-7 border-b-2 border-purple-light py-1 mb-2 items-center">
+                <h2 className="flex w-[50%] pl-3">Resultado Pedido</h2>
+                <div className="w-full flex w-[50%] justify-evenly">
+                    <a href="#" className="flex items-center justify-center h-[32px] min-w-[32px] rounded-full bg-purple-medium"><img src={verIcon} alt="ver-resultado" className="w-5 h-5" /></a>
+                    <a href="#" className="flex items-center justify-center h-[32px] min-w-[32px] rounded-full bg-purple-medium"><img src={editarIcon} alt="editar-resultado" className="w-5 h-5" /></a>
+                    <a href="#" className="flex items-center justify-center h-[32px] min-w-[32px] rounded-full bg-purple-medium"><img src={deletarIcon} alt="deletar-resultado" className="w-5 h-5" /></a>
+                    <a href="#" className="flex items-center justify-center"><img src={aprovarIcon} alt="aprovar-pedido" className="w-8 h-8" /></a>
+                </div>
+            </li>
+    )
+}
