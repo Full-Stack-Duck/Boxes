189b9bfeac3a65a0d5191ea00f3acf73426975a4
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-15 17:27:56 -0300

    CadastrarPedidos criado, completo

---
+++ src/components/CadastrarPedidos.tsx
@@ -0,0 +1,64 @@
+import clienteIcon from '../assets/costumer-icon-on.svg'
+
+export function CadastrarPedidos(){
+    return(
+        <section className="flex-col px-7 my-1 bg-white font-quicksand font-semibold border-y"> 
+            <div>
+                <h3 className="text-lg py-2">Cadastrar Pedido ou Orçamento</h3>
+                <form className="text-sm text-purple-medium">
+
+                    <div className="flex justify-around text-xs text-purple-medium py-2 mb-2">
+                        <div className="flex items-center gap-x-1">
+                            <input type="radio" name="categoria" value="estocavel" />
+                            <label htmlFor="">ORÇAMENTO</label>
+                        </div>
+                        <div className="flex items-center gap-x-1">
+                            <input type="radio" name="categoria" value="naoestocavel" />
+                            <label htmlFor="">PEDIDO</label>
+                        </div>
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="">NOME DO CLIENTE</label>
+                        <input type="text" className="w-full h-8 border rounded border-purple-light" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="email">E-MAIL</label>
+                        <input type="email" id="email" name="email" placeholder="ex: cliente123@email.com" className="w-full h-8 border rounded border-purple-light placeholder:p-2 placeholder:italic placeholder:text-gray-lightgray" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="telefone">TELEFONE</label>
+                        <input type="tel" id="telefone" name="telefone" placeholder="ex: (DDD) 0 0000-0000" className="w-full h-8 border rounded border-purple-light placeholder:p-2 placeholder:italic placeholder:text-gray-lightgray" />
+                    </div>
+
+                    <div className="mb-4">
+                        <label htmlFor="">DATA DA DESPESA</label>
+                        <input type="date" id="data" name="data" className="w-full h-8 border rounded border-purple-light" />
+                    </div>
+
+                    <div className="flex justify-around text-xs text-purple-medium py-2 mb-2">
+                        <div className="flex items-center gap-x-1">
+                            <input type="checkbox" name="categoria" value="estocavel" />
+                            <label htmlFor="">RETIRADA NO LOCAL</label>
+                        </div>
+                        <div className="flex items-center gap-x-1">
+                            <input type="checkbox" name="categoria" value="naoestocavel" />
+                            <label htmlFor="">ENTREGA</label>
+                        </div>
+                    </div>
+
+                    <div className="flex flex-wrap items-center text-sm mb-5">
+                        <label htmlFor="">ENDEREÇO DA ENTREGA</label>
+                        <textarea className="w-full h-16 resize-none over-flow-y-auto border rounded placeholder:text-xs placeholder:p-2 placeholder:italic placeholder:text-gray-lightgray" id="" name="" placeholder="Ex: Rua, nº, Bairro, complemento" />
+                    </div>
+
+                    <button type="submit" className="flex items-center justify-center w-full h-10 mt-2 mb-4 border-2 rounded  bg-purple-medium border-purple-dark shadow-md shadow-purple-shadow text-white text-base"> CADASTRAR CLIENTE
+                        <img className="mx-1" src={clienteIcon} alt="" />
+                    </button>
+                </form>
+            </div>
+        </section>
+    )
+}
