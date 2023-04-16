582d3fb6e73055f286a2cea59cba22bd6a24c509
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-16 11:20:51 -0300

    botão cadastrar apagado + border modificada

--- src/components/CadastrarPedidos.tsx
+++ src/components/CadastrarPedidos.tsx
@@ -2,7 +2,7 @@ import clienteIcon from '../assets/costumer-icon-on.svg'
 
 export function CadastrarPedidos(){
     return(
-        <section className="flex-col px-7 my-1 bg-white font-quicksand font-semibold border-y"> 
+        <section className="flex-col px-7 my-1 bg-white font-quicksand font-semibold border-t"> 
             <div>
                 <h3 className="text-lg py-2">Cadastrar Pedido ou Orçamento</h3>
                 <form className="text-sm text-purple-medium">
@@ -53,10 +53,6 @@ export function CadastrarPedidos(){
                         <label htmlFor="">ENDEREÇO DA ENTREGA</label>
                         <textarea className="w-full h-16 resize-none over-flow-y-auto border rounded placeholder:text-xs placeholder:p-2 placeholder:italic placeholder:text-gray-lightgray" id="" name="" placeholder="Ex: Rua, nº, Bairro, complemento" />
                     </div>
-
-                    <button type="submit" className="flex items-center justify-center w-full h-10 mt-2 mb-4 border-2 rounded  bg-purple-medium border-purple-dark shadow-md shadow-purple-shadow text-white text-base"> CADASTRAR CLIENTE
-                        <img className="mx-1" src={clienteIcon} alt="" />
-                    </button>
                 </form>
             </div>
         </section>
