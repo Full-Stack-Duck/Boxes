771f5c44c0d8e676f4aff9262814856af7507949
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-04-19 10:23:34 -0300

    corrigindo css

--- src/components/ResultadoPedido.tsx
+++ src/components/ResultadoPedido.tsx
@@ -8,7 +8,7 @@ export function ResultadoPedido(){
     return(
             <li className="flex mx-7 border-b-2 border-purple-light py-1 mb-2 items-center">
                 <h2 className="flex w-[50%] pl-3">Resultado Pedido</h2>
-                <div className="w-full flex w-[50%] justify-evenly">
+                <div className="flex w-[50%] justify-evenly">
                     <a href="#" className="flex items-center justify-center h-[32px] min-w-[32px] rounded-full bg-purple-medium"><img src={verIcon} alt="ver-resultado" className="w-5 h-5" /></a>
                     <a href="#" className="flex items-center justify-center h-[32px] min-w-[32px] rounded-full bg-purple-medium"><img src={editarIcon} alt="editar-resultado" className="w-5 h-5" /></a>
                     <a href="#" className="flex items-center justify-center h-[32px] min-w-[32px] rounded-full bg-purple-medium"><img src={deletarIcon} alt="deletar-resultado" className="w-5 h-5" /></a>
