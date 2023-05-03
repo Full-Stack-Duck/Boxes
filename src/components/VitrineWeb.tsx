656cab75d0d77ec0689739488d79b3df4d70cccc
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-05-03 19:39:09 -0300

    criando component

---
+++ src/components/VitrineWeb.tsx
@@ -0,0 +1,12 @@
+
+
+export function Vitrine(){
+    return(
+        <article>
+            <h1>Organize o seu negócio para vender mais com Boxes</h1>
+            <p>O sistema de gestão feito para organizar e integrar as etapas de seu negócio para você dedicar mais tempo pensando fora da caixa</p>
+            <img src="" alt="ilustração-dois-homens-discutindo-ideias" />
+            <button>Assine Grátis!</button>
+        </article>
+    )
+}
