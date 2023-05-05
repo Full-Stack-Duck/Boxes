71f89c1e5125f9c40e11bc8e708f17e9231b9470
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-05-05 00:51:56 -0300

    criando component Beneficios

---
+++ src/components/BeneficiosWeb.tsx
@@ -0,0 +1,28 @@
+
+
+export function Beneficios(){
+    return(
+        <section>
+            <h2>BENEFICIOS</h2>
+            <p>Veja como o sistema de gestão </p>
+            <span>Boxes</span>
+            <p>irá ajudar você e sua empresa.</p>
+            <div>
+                <h3>GESTÃO DE TEMPO</h3>
+                <p>Com o conceito “All-in-One” (Tudo-em-Um, do inglês), você terá ao alcance todas as etapas de seu negócio integradas em um só aplicativo, possibilitando um melhor investimento de seu tempo</p>
+            </div>
+            <div>
+                <h3>AGILIDADE NA VENDA</h3>
+                <p>A agilidade no momento de produzir um orçamento e enviar para o cliente, de forma clara e de fácil leitura, aumenta as suas chances de fechar os contratos de venda, além de repercussão do excelente atendimento ao cliente.</p>
+            </div>
+            <div>
+                <h3>CONTROLE FINANCEIRO</h3>
+                <p>Conquiste um controle maior de suas finanças - despesas categorizadas, faturamentos, contas a receber e um painel comparativo compõem as funções do sistema.</p>
+            </div>
+            <div>
+                <h3>PRODUTOS EM ESTOQUE</h3>
+                <p>Cadastre seus produtos e mantenha controle de seu estoque real conforme sua produção e o escoamento do produto através dos pedidos realizados. Saiba exatamente a quantidade de cada produto você tem para pronta-entrega.</p>
+            </div>
+        </section>
+    )
+}
