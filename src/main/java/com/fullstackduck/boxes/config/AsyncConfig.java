5bd75584cc5b36d83cc56dd0b5133ceb836e52e5
ViniciusDevAssis <viniciusfsa2014@gmail.com>
2023-05-27 10:42:01 -0300

    Correção nos métodos da entidade ItensOrcamento

--- src/main/java/com/fullstackduck/boxes/config/AsyncConfig.java
+++ src/main/java/com/fullstackduck/boxes/config/AsyncConfig.java
@@ -3,11 +3,25 @@ package com.fullstackduck.boxes.config;
 import java.lang.reflect.Method;
 
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 @Configuration
+@EnableAsync
 public class AsyncConfig implements AsyncConfigurer{
+	
+	@Bean(name = "asyncTaskExecutor")
+    public ThreadPoolTaskExecutor asyncTaskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(5); // Defina o tamanho do pool de threads
+        executor.setMaxPoolSize(10); // Defina o tamanho máximo do pool de threads
+        executor.setThreadNamePrefix("AsyncTask-"); // Prefixo do nome das threads
+        executor.initialize();
+        return executor;
+    }
 	
 	public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
 		return new AsyncUncaughtExceptionHandler() {
