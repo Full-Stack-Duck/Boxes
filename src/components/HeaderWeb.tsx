60e0297444eb6f7253e48221f59c6f035e424e36
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-06-17 12:53:07 -0300

    botao entrar criado + responsividade + ajustes de padding

--- src/components/HeaderWeb.tsx
+++ src/components/HeaderWeb.tsx
@@ -3,17 +3,20 @@ import logoIcon from '../assets/logoIcon.svg'
 
 export function HeaderWeb(){
     return(
-        <header className="flex px-7 justify-between items-center bg-purple-dark w-full h-16 ">
-        <div className="w-[50%] flex items-center justify-around ">
-          <a href="" className="">
+        <header className="flex px-7 justify-between items-center bg-purple-dark w-full h-16 xl:px-32">
+        <div className="w-[50%] flex items-center">
+          <a href="" className="pr-4">
             <img src={logoIcon} alt="Logomarca da Manager Box" />
           </a>
           <div className="w-0.5 h-9 bg-purple-light"></div>
-          <h1 className="font-viga text-2xl tracking-widest text-white">Boxes</h1>
+          <h1 className="font-viga text-2xl tracking-widest text-white pl-4">Boxes</h1>
         </div>
         <div className="w-[50%] flex items-center justify-end">
           <div className="mr-3">
             <a href="" className="text-white font-quicksand text-sm tracking-wider px-3 py-1 bg-purple-medium border rounded-md border-white">Cadastrar</a>
+          </div>
+          <div className="mr-3 hidden md:block">
+            <a href="" className="text-white font-quicksand text-sm tracking-wider px-7 py-1 bg-purple-dark border rounded-md border-white">Entrar</a>
           </div>
           <button className="hover:bg-purple-400 hover:bg-opacity-10 rounded-full">
             <DotsThreeVertical size={32} weight="duotone" className="text-[#fff]"/>
@@ -21,5 +24,4 @@ export function HeaderWeb(){
     
         </div>
         </header>
-    )
-}
+    )}
