e2eed279748b2c91283128884382abeece69fd64
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-06-18 13:47:46 -0300

    responsividade ok

--- src/components/CadastrarUsuario.tsx
+++ src/components/CadastrarUsuario.tsx
@@ -83,125 +83,126 @@ export function CadastrarUsuario(){
           };
 
     return (
-        <div className="flex justify-center px-7 w-full">
-            <div className="flex flex-col w-[23rem] sm:w-[50rem] h-fit justify-center items-center">
-            <div>
-                <article className="flex flex-col justify-center items-center mb-3">
+        <div className="flex justify-center w-full">
+            <div className="hidden bg-purple-medium lg:block lg:w-1/2 lg:rounded-r-3xl"></div>
+            <div className="flex flex-col w-[23rem] sm:w-[50rem] h-fit justify-center items-center lg:px-32">
+                <div>
+                    <article className="flex flex-col justify-center items-center mb-3">
+                        <div>
+                            <img src={logoIcon} alt="logomarca"/>
+                        </div>
+                        <h1 className="font-semibold tracking-wider text-2xl">Seja bem-vindo</h1>
+                        <h3 className="text-xs text-gray-lightgray">Por favor, insira seus dados</h3>
+                    </article>
+                </div>
+                <form 
+                className='flex flex-col gap-2 w-full px-5'
+                onSubmit={handleSubmit(criarUsuario)}
+                >
+                    <div className='flex items-center gap-4 justify-between w-full flex-wrap sm:justify-center sm:gap-28'>
+                        <div className='flex gap-2 items-center flex-wrap'>
+                            <input 
+                            type="radio" 
+                            id="person1" 
+                            className={styles.inputRadio} 
+                            {...register('tipoDePessoa')} 
+                            value='Pessoa Fisica'
+                            onChange={() => handlePersonTypeChange()}
+                            checked={radioOption}
+                            />
+                            <label 
+                            htmlFor="person1" 
+                            className='cursor-pointer text-purple-medium uppercase font-semibold'
+                            >
+                                Pessoa Física
+                                </label>
+                        </div>
+                        <div className='flex gap-2 items-center flex-wrap'>
+                            <input 
+                            type="radio"  
+                            id="person2" 
+                            className={styles.inputRadio} 
+                            {...register('tipoDePessoa')} 
+                            value='Pessoa Juridica'
+                            onChange={() => handlePersonTypeChange()}
+                            />
+                            <label 
+                            htmlFor="person2" 
+                            className='cursor-pointer text-purple-medium uppercase font-semibold'
+                            >
+                                Pessoa Jurídica
+                                </label>
+                        </div>
+                    </div>
+
                     <div>
-                    <img src={logoIcon} alt="logomarca"/>
+                        <label htmlFor="email" className={styles.labelsStyles}>E-mail:</label>
+                        <input type="text" placeholder='E-mail' className={styles.inputStyles} {...register('email')} onChange={(e) => setEmail(e.target.value)}/>
                     </div>
-                    <h1 className="font-semibold tracking-wider text-2xl">Seja bem-vindo</h1>
-                    <h3 className="text-xs text-gray-lightgray">Por favor, insira seus dados</h3>
-                </article>
-            </div>
-            <form 
-            className='flex flex-col gap-2 w-full px-5'
-            onSubmit={handleSubmit(criarUsuario)}
-            >
-                <div className='flex items-center gap-4 justify-between w-full flex-wrap sm:justify-center sm:gap-28'>
-                    <div className='flex gap-2 items-center flex-wrap'>
+                    <span className='text-xs text-red-600 h-3'>{errors.email && errors.email.message}</span>
+
+                    <div>
+                        <label htmlFor="nome" className={styles.labelsStyles}>Nome de usuário:</label>
                         <input 
-                        type="radio" 
-                        id="person1" 
-                        className={styles.inputRadio} 
-                        {...register('tipoDePessoa')} 
-                        value='Pessoa Fisica'
-                        onChange={() => handlePersonTypeChange()}
-                        checked={radioOption}
+                        type="text" 
+                        placeholder='Nome' 
+                        className={styles.inputStyles} 
+                        {...register('nome')}
+                        onChange={(e) => setNome(e.target.value)}
                         />
-                        <label 
-                        htmlFor="person1" 
-                        className='cursor-pointer text-purple-medium uppercase font-semibold'
-                        >
-                            Pessoa Física
-                            </label>
                     </div>
-                    <div className='flex gap-2 items-center flex-wrap'>
+                    <span className='text-xs text-red-600 h-3'>{errors.nome && errors.nome.message}</span>
+
+                    <div>
+                        <label 
+                        htmlFor="documento" 
+                        className={styles.labelsStyles}>{radioOption? "CPF" : "CNPJ"}
+                            <span className='text-xs'>
+                            (apenas números)
+                            :
+                            </span>
+                        </label> 
                         <input 
-                        type="radio"  
-                        id="person2" 
-                        className={styles.inputRadio} 
-                        {...register('tipoDePessoa')} 
-                        value='Pessoa Juridica'
-                        onChange={() => handlePersonTypeChange()}
-                        />
-                        <label 
-                        htmlFor="person2" 
-                        className='cursor-pointer text-purple-medium uppercase font-semibold'
-                        >
-                            Pessoa Jurídica
-                            </label>
+                        type="text" 
+                        placeholder={radioOption? 'Ex.: 000.000.000-00' : 'Ex.: 00.000.000/0001-00'} 
+                        className={styles.inputStyles} 
+                        {...register('documento')}
+                        onChange={(e) => setDocumento(e.target.value)}
+                        />    
                     </div>
-                </div>
+                    <span className='text-xs text-red-600 h-3'>{errors.documento && errors.documento.message}</span>
+                        
+                    <div>
+                        <label htmlFor="senha" className={styles.labelsStyles}>Senha:</label>
+                        <input type="password" placeholder='Senha' className={styles.inputStyles} {...register('senha')} onChange={(e) => setSenha(e.target.value)}/>
+                    </div>
+                    <span className='text-xs text-red-600 h-3'>{errors.senha && errors.senha.message}</span>
 
-                <div>
-                    <label htmlFor="email" className={styles.labelsStyles}>E-mail:</label>
-                    <input type="text" placeholder='E-mail' className={styles.inputStyles} {...register('email')} onChange={(e) => setEmail(e.target.value)}/>
-                </div>
-                <span className='text-xs text-red-600 h-3'>{errors.email && errors.email.message}</span>
+                    <div>
+                        <label htmlFor="senhaConfirmada" className={styles.labelsStyles}>Confirme sua senha:</label>
+                        <input type="password" placeholder='Confirmar Senha' className={styles.inputStyles} {...register('senhaConfirmada')}/>
+                    </div>
+                    <span className='text-xs text-red-600 h-3'>{errors.senhaConfirmada && errors.senhaConfirmada.message}</span>
 
-                <div>
-                    <label htmlFor="nome" className={styles.labelsStyles}>Nome de usuário:</label>
-                    <input 
-                    type="text" 
-                    placeholder='Nome' 
-                    className={styles.inputStyles} 
-                    {...register('nome')}
-                    onChange={(e) => setNome(e.target.value)}
-                    />
-                </div>
-                <span className='text-xs text-red-600 h-3'>{errors.nome && errors.nome.message}</span>
+                    <div className='flex gap-1 items-center justify-start'>
+                        <input type="checkbox" id="aceitar-termos" {...register('termoDeUso')} className={styles.checkboxStyle}/>
+                        <label htmlFor="aceitar-termos" className={styles.labelsStyles && "pl-3 font-nunito text-sm"}>Eu li e concordo com os <a href='https://www.youtube.com/' className='text-purple-medium underline underline-offset-1'>Termos de Uso</a></label>
+                    </div>
+                    <span className='text-xs text-red-600 whitespace-nowrap h-4'>{errors.termoDeUso && errors.termoDeUso.message}</span>
 
-                <div>
-                    <label 
-                    htmlFor="documento" 
-                    className={styles.labelsStyles}>{radioOption? "CPF" : "CNPJ"}
-                        <span className='text-xs'>
-                        (apenas números)
-                        :
-                        </span>
-                    </label> 
-                    <input 
-                    type="text" 
-                    placeholder={radioOption? 'Ex.: 000.000.000-00' : 'Ex.: 00.000.000/0001-00'} 
-                    className={styles.inputStyles} 
-                    {...register('documento')}
-                    onChange={(e) => setDocumento(e.target.value)}
-                    />    
-                </div>
-                <span className='text-xs text-red-600 h-3'>{errors.documento && errors.documento.message}</span>
-                    
-                <div>
-                    <label htmlFor="senha" className={styles.labelsStyles}>Senha:</label>
-                    <input type="password" placeholder='Senha' className={styles.inputStyles} {...register('senha')} onChange={(e) => setSenha(e.target.value)}/>
-                </div>
-                <span className='text-xs text-red-600 h-3'>{errors.senha && errors.senha.message}</span>
+                <button 
+                type="submit" 
+                className='flex justify-center items-center disabled:bg-purple-medium/80 disabled:hover:bg-purple-900/70 bg-purple-medium border py-2 w-full rounded-xl hover:bg-purple-900 text-white font-bold mt-1'
+                disabled={isSendingFeedback}
+                >
+                    { isSendingFeedback ? <Loading /> :'Avançar' }
+                </button>
+                </form>
 
-                <div>
-                    <label htmlFor="senhaConfirmada" className={styles.labelsStyles}>Confirme sua senha:</label>
-                    <input type="password" placeholder='Confirmar Senha' className={styles.inputStyles} {...register('senhaConfirmada')}/>
-                </div>
-                <span className='text-xs text-red-600 h-3'>{errors.senhaConfirmada && errors.senhaConfirmada.message}</span>
+                <span className='text-purple-stroke text-sm mt-9'>Já tem uma conta? <a href="_blank" className='font-semibold text-purple-medium text-base' >FAÇA O LOGIN</a></span>
 
-                <div className='flex gap-1 items-center justify-start'>
-                    <input type="checkbox" id="aceitar-termos" {...register('termoDeUso')} className={styles.checkboxStyle}/>
-                    <label htmlFor="aceitar-termos" className={styles.labelsStyles && "pl-3 font-nunito text-sm"}>Eu li e concordo com os <a href='https://www.youtube.com/' className='text-purple-medium underline underline-offset-1'>termos de uso</a></label>
-                </div>
-                <span className='text-xs text-red-600 whitespace-nowrap h-4'>{errors.termoDeUso && errors.termoDeUso.message}</span>
-
-            <button 
-            type="submit" 
-            className='flex justify-center items-center disabled:bg-purple-medium/80 disabled:hover:bg-purple-900/70 bg-purple-medium border py-2 w-full rounded-xl hover:bg-purple-900 text-white font-bold mt-1'
-            disabled={isSendingFeedback}
-            >
-                { isSendingFeedback ? <Loading /> :'Avançar' }
-            </button>
-            </form>
-
-            <span className='text-purple-stroke text-sm mt-9'>Já tem uma conta? <a href="_blank" className='font-semibold text-purple-medium text-base' >FAÇA O LOGIN</a></span>
-
-            <a ></a>
-            <pre className='bg-purple-dark text-white font-bold text-sm mt-8'>{output}</pre>
+                <a ></a>
+                <pre className='bg-purple-dark text-white font-bold text-sm mt-8'>{output}</pre>
             </div>
             
 
