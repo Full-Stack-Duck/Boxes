52a2d5e8a15afc45405346ee4b96c96dfce39b1a
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-06-18 14:07:08 -0300

    responsividade ok

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,13 +4,13 @@ import UserIcon from '../assets/user-icon.svg'
 
 export function Header(){
     return(
-        <header className="flex px-7 justify-between items-center bg-purple-dark w-full h-16 ">
-        <div className="w-[50%] flex items-center justify-around gap-4">
-          <a href="_blank" className="h-11 w-11">
+        <header className="flex px-7 justify-between items-center bg-purple-dark w-full h-16 lg:px-32">
+        <div className="w-[50%] flex items-center">
+          <a href="_blank" className="pr-4">
             <img src={logoIcon} alt="Logomarca Boxes" />
           </a>
-          <div className="w-[1px] h-9 bg-purple-light"></div>
-          <h1 className="font-viga text-2xl tracking-widest text-white">Boxes</h1>
+          <div className="w-0.5 h-9 bg-purple-light"></div>
+          <h1 className="font-viga text-2xl tracking-widest text-white pl-4">Boxes</h1>
         </div>
         <div className="w-[50%] flex items-center justify-end">
           <a href="#" className="hover:bg-purple-400 hover:bg-opacity-10 rounded-full">
