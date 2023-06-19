fc27a8e96602176f93947763e4bb0408487eda00
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-06-19 17:59:03 -0300

    responsividade ok

--- src/components/ClientesCadastrados.tsx
+++ src/components/ClientesCadastrados.tsx
@@ -2,8 +2,8 @@ import client_plus_icon from "../assets/client_plus_icon.svg";
 
 export function ClientesCadastrados() {
   return (
-    <section className="flex flex-col w-full items-center">
-      <div className="flex flex-col px-7 items-start w-[23rem] sm:w-[50rem] border-b-2">
+    <section className="flex flex-col">
+      <div className="flex flex-col px-7 items-start border-b-2 lg:px-0">
         <h1 className=" font-quicksand font-bold text-lg pt-3">
           Clientes Cadastrados
         </h1>
@@ -21,10 +21,10 @@ export function ClientesCadastrados() {
           <p className="text-purple-medium text-3xl font-bold">5</p>
         </div>
         <div className="w-full flex justify-between items-center pb-5">
-          <p className="text-xs text-gray-400">
+          <p className="text-xs text-gray-400 lg:pr-6">
             *Dentro do período selecionado
           </p>
-          <a className="flex items-center justify-center w-10 h-10 rounded-full border border-purple-medium">
+          <a className="flex items-center justify-center w-10 h-10 border-purple-medium">
             <img src={client_plus_icon} className="w-7 h-7" />
           </a>
         </div>
