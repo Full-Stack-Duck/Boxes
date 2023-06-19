0a811fffe07c420433804a2b656f8ea3e8e677ff
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-06-19 13:41:35 -0300

    responsividade ok

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -51,60 +51,65 @@ export function Login(){
     }
 
     return(
-        <section className="font-quicksand">
-            <article className=" flex flex-col justify-center items-center">
-                    <img src={logoIcon} alt="logomarca" className="w-20 h-20 mb-6 mt-10" />
-                    <h1 className="font-semibold tracking-wider text-2xl">Seja bem-vindo</h1>
-                    <h3 className="text-xs text-gray-lightgray pb-12">Por favor, insira seus dados</h3>
-            </article>
-            <article className="flex-col mx-5">
-                <form onSubmit={handleSubmit(entrar)}>
-                    <div className="py-1.5 font-medium">
-                        <label htmlFor="">Email</label>
-                        <input type="email"
-                        className="w-full h-8 border rounded border-purple-stroke placeholder:text-xs placeholder:px-3 placeholder:font-nunito" 
-                        placeholder="Escreva seu email"
-                        {...register('email')} 
-                        onChange={(e) => setEmail(e.target.value)}
-                        />
-                    </div>
-                    
-                <span className='text-xs text-red-600 h-3'>{errors.email && errors.email.message}</span>
+        <section className="font-quicksand flex justify-center w-full">
+            <div className="hidden bg-purple-medium lg:block lg:w-1/2 lg:rounded-r-3xl"></div>
 
-                    <div className="py-1.5 font-medium">
-                        <label htmlFor="">Senha</label>
-                        <input type="password" className="w-full h-8 border rounded border-purple-stroke"
-                        {...register('senha')} 
-                        onChange={(e) => setSenha(e.target.value)}
-                        />
-                        <span className='text-xs text-red-600 h-3'>{errors.senha && errors.senha.message}</span>
-                    </div>
-
-                    <div className="flex py-1.5 font-nunito text-xs">
-                        <div className="w-[50%]">
-                            <input type="checkbox" className="mr-2 border rounded border-purple-stroke"/>
-                            <label htmlFor="">Salvar senha</label>
+            <div className="flex flex-col w-[23rem] sm:w-[50rem] h-fit justify-center items-center lg:px-32">
+                <article className=" flex flex-col justify-center items-center">
+                        <img src={logoIcon} alt="logomarca" className="w-20 h-20 mb-6 mt-10" />
+                        <h1 className="font-semibold tracking-wider text-2xl">Seja bem-vindo</h1>
+                        <h3 className="text-xs text-gray-lightgray pb-12">Por favor, insira seus dados</h3>
+                </article>
+                <article className="flex-col mx-5">
+                    <form onSubmit={handleSubmit(entrar)}>
+                        <div className="py-1.5 font-medium">
+                            <label htmlFor="">Email</label>
+                            <input type="email"
+                            className="w-full h-8 border rounded border-purple-stroke placeholder:text-xs placeholder:px-3 placeholder:font-nunito" 
+                            placeholder="Escreva seu email"
+                            {...register('email')} 
+                            onChange={(e) => setEmail(e.target.value)}
+                            />
                         </div>
-                        <div className="w-[50%] flex justify-end">
-                            <a href="#" className="text-sm text-purple-medium">
-                                Esqueci minha senha
-                            </a>
-                        </div>
-                    </div>
-                    <button
-                    type='submit'
-                    className="flex items-center justify-center w-full h-10 mt-2 mb-4 rounded-xl  bg-purple-medium shadow-md shadow-purple-shadow text-white tracking-widest"
-                    disabled={isLogging}
-                    >
-                { isLogging ? <Loading /> :'Entrar' }
                         
-                    </button>
-                </form>
-                <div className="flex justify-center gap-2 py-4 items-center">
-                    <p className="text-xs text-gray-lightgray">Não tem uma conta?</p>
-                    <a href="#" className="text-sm text-purple-medium">CADASTRE-SE</a>
-                </div>
-            </article>
+                    <span className='text-xs text-red-600 h-3'>{errors.email && errors.email.message}</span>
+
+                        <div className="py-1.5 font-medium">
+                            <label htmlFor="">Senha</label>
+                            <input type="password" className="w-full h-8 border rounded border-purple-stroke"
+                            {...register('senha')} 
+                            onChange={(e) => setSenha(e.target.value)}
+                            />
+                            <span className='text-xs text-red-600 h-3'>{errors.senha && errors.senha.message}</span>
+                        </div>
+
+                        <div className="flex py-1.5 font-nunito text-xs">
+                            <div className="w-[50%]">
+                                <input type="checkbox" className="mr-2 border rounded border-purple-stroke"/>
+                                <label htmlFor="">Salvar senha</label>
+                            </div>
+                            <div className="w-[50%] flex justify-end">
+                                <a href="#" className="text-sm text-purple-medium">
+                                    Esqueci minha senha
+                                </a>
+                            </div>
+                        </div>
+                        <button
+                        type='submit'
+                        className="flex items-center justify-center w-full h-10 mt-2 mb-4 rounded-xl  bg-purple-medium shadow-md shadow-purple-shadow text-white tracking-widest"
+                        disabled={isLogging}
+                        >
+                    { isLogging ? <Loading /> :'Entrar' }
+                            
+                        </button>
+                    </form>
+                    <div className="flex justify-center gap-2 py-4 items-center">
+                        <p className="text-xs text-gray-lightgray">Não tem uma conta?</p>
+                        <a href="#" className="text-sm text-purple-medium">CADASTRE-SE</a>
+                    </div>
+                </article>
+                
+            </div>
         </section>
     )
 }
