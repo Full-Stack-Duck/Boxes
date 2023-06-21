b1bfdd3227bdc27d0f01955655f42ee674e22f37
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-06-21 17:06:48 -0300

    criado e responsivo

---
+++ src/components/CabecalhoCliente.tsx
@@ -0,0 +1,9 @@
+
+export function CabecalhoCliente(){
+    return(
+      <div className="w-full flex items-center justify-between py-4 border-b-2">
+        <p className="font-quicksand font-bold pl-7 text-2xl lg:pl-32">Clientes</p>
+      </div>
+        
+    )
+}
