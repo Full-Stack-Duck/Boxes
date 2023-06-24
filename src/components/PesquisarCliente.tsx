192dbe58f943dff45a9907ca8900e6d4f1ad6c1d
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-06-24 13:20:04 -0300

    inserindo <ResultadoCliente /> no component, sem lógica

--- src/components/PesquisarCliente.tsx
+++ src/components/PesquisarCliente.tsx
@@ -12,16 +12,10 @@ import { ChangeEvent, useEffect, useState } from "react";
 import Dinero from 'dinero.js';
 
 const enumType = [
-  "Selecione o tipo...",
-  "FRITO",
-  "FORNO",
-  "PRATO",
-  "BEBIDA",
-  "SOBREMESA",
-  "DOCE",
-  "ACOMPANHAMENTO",
-  "BEBIDA_NE",
-  "BOLOS E TORTAS",
+  "Selecione o filtro...",
+  "NOME",
+  "EMAIL",
+  "TELEFONE",
 ] as const;
 // FRITO(1),
 // FORNO(2),
@@ -128,66 +122,53 @@ function filtrarProdutosPor(
     <section className="w-full flex justify-center border-y bg-white font-quicksand font-semibold lg:w-1/2 lg:border-purple-light lg:border lg:rounded-xl lg:ml-5">
       <div className="flex-col items-center px-7 w-full">
         <div>
-            <h3 className="text-lg py-2">Pesquisar produto</h3>
+          <h3 className="text-lg py-2">Pesquisar Cliente</h3>
           <article className="flex-row text-sm text-purple-medium items-center">
-            <form >
+            <form>
               <div className="flex flex-col">
-                <label htmlFor="">PESQUISAR CATEGORIA:</label>
+                
+                <label htmlFor="">PESQUISAR POR:</label>
                 <select
                   className="border rounded font-nunito h-8 mb-4"
-                  onChange={(e) => handleCategoryChange(e)}
+                  onChange={(e) => handleTypeChange(e)}
                   >
-                  {enumCategory.map((field, index) => {
+                  {enumType.map((field, index) => {
                     return (
                       <option
                       className="text-purple-light text-xs"
                       key={index.toString()}
-                      value={index}
+                      value={field}
                       >
                         {field}
                       </option>
                     );
                   })}
                 </select>
-              <label htmlFor="">PESQUISAR TIPO:</label>
-              <select
-                      className="border rounded font-nunito h-8 mb-4"
-                      onChange={(e) => handleTypeChange(e)}
-                      >
-                      {enumType.map((field, index) => {
-                        return (
-                          <option
-                          className="text-purple-light text-xs"
-                          key={index.toString()}
-                          value={field}
-                          >
-                            {field}
-                          </option>
-                        );
-                      })}
-                    </select>
-              <label htmlFor="">PALAVRA-CHAVE:</label>
-              <input
-                className="border rounded h-8 border-purple-light"
-                type="text"
-                />
-            <button
-              className="flex items-center justify-center w-full h-10 mt-4 mb-4 border-2 rounded  bg-purple-medium border-purple-dark shadow-md shadow-purple-shadow text-white"
-              type="button"
-              onClick={() => filtrarProdutosPor(category)}
-              >
-              PESQUISAR
-              <img
-                className="w-5 h-5 mx-1 bg-purple-medium"
-                src={searchIcon}
-                alt="lupa_de_pesquisa"
-                />
-            </button>
-            </div>
+                <label htmlFor="">PALAVRA-CHAVE:</label>
+                <input
+                  className="border rounded h-8 border-purple-light"
+                  type="text"
+                  />
+                <button
+                className="flex items-center justify-center w-full h-10 mt-4 mb-4 border-2 rounded  bg-purple-medium border-purple-dark shadow-md shadow-purple-shadow text-white"
+                type="button"
+                onClick={() => filtrarProdutosPor(category)}
+                >
+                PESQUISAR
+                  <img
+                    className="w-5 h-5 mx-1 bg-purple-medium"
+                    src={searchIcon}
+                    alt="lupa_de_pesquisa"
+                    />
+                </button>
+              </div>
             </form>
           </article>
-          <div className="flex justify-end">
-          </div>
+        </div>
+
+        <div className="">
+          <ResultadoCliente />
+          <ResultadoCliente />
         </div>
         
       </div>
