5152248494dd7524f37a6ead528e156dbb59f601
Bruno Marques Goncalves <bruno.marques.goncalves@gmail.com>
2023-06-24 15:08:05 -0300

    componentes criados

---
+++ src/components/CabecalhoProduto.tsx
@@ -0,0 +1,9 @@
+
+export function CabecalhoProduto(){
+    return(
+      <div className="w-full flex items-center justify-between py-4 border-b-2">
+        <p className="font-quicksand font-bold pl-7 text-2xl lg:pl-32">Produtos</p>
+      </div>
+        
+    )
+}
